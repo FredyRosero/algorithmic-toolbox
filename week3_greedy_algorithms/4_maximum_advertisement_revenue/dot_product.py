531d83b4193a860edf23030f35b8228ef3fe437e
FredyRosero <faroseroc@unal.edu.co>
2021-12-31 01:21:36 -0500

    3-4

--- week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.py
+++ week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.py
@@ -2,18 +2,15 @@
 
 import sys
 
-def max_dot_product(a, b):
-    #write your code here
-    res = 0
-    for i in range(len(a)):
-        res += a[i] * b[i]
-    return res
+def max_dot_product(a, b, n):
+    a.sort()
+    b.sort()
+    ans = sum([a[i]*b[i] for i in range(n)])
+    return ans
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n = data[0]
-    a = data[1:(n + 1)]
-    b = data[(n + 1):]
-    print(max_dot_product(a, b))
+    n = int(sys.stdin.readline())
+    a = list(map(int, sys.stdin.readline().split()))
+    b = list(map(int, sys.stdin.readline().split()))
+    print(max_dot_product(a, b, n))
     
