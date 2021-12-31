4f4101da773b64dbc5ce9d68257052077dadcde4
FredyRosero <faroseroc@unal.edu.co>
2021-12-31 16:48:11 -0500

    5-2

--- week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
+++ week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
@@ -2,20 +2,36 @@
 import sys
 
 def optimal_sequence(n):
-    sequence = []
-    while n >= 1:
-        sequence.append(n)
-        if n % 3 == 0:
-            n = n // 3
-        elif n % 2 == 0:
-            n = n // 2
+    sequence = [None]*(n+1)
+    sequence[0] = 0
+    sequence[1] = 0
+    if n >=2 :
+        for i in range(2,n+1) :
+            if(i/2 == i//2 and i/3 == i//3):
+                sequence[i] = min (sequence[i//2],sequence[i//3],sequence[i-1])+1
+            elif(i/2 == i//2 and  not (i/3 == i//3)):
+                sequence[i] =min (sequence[i//2],sequence[i-1]) +1
+            elif(not (i/2 == i//2) and  i/3 == i//3):
+                sequence[i] = min (sequence[i//3],sequence[i-1])+1
+            else :
+                sequence[i] = sequence[i-1] +1 
+    items = list()
+    items.append(n)
+    while n>1:
+        if(n/3 == n//3 and sequence[n//3] ==sequence[n]-1)  :
+            items.append(n//3)
+            n = n//3
+        elif(n/2 == n//2 and sequence[n//2] ==sequence[n]-1)  :
+            items.append(n//2)
+            n = n//2
         else:
-            n = n - 1
-    return reversed(sequence)
+            items.append(n-1)
+            n-=1
+    return sequence[-1],reversed(items)      
 
-input = sys.stdin.read()
-n = int(input)
+n = int(sys.stdin.readline())
 sequence = list(optimal_sequence(n))
-print(len(sequence) - 1)
+n1,sequence = (optimal_sequence(n))
+print(n1)
 for x in sequence:
     print(x, end=' ')
