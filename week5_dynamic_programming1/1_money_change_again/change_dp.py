72e8b742fc8541af432a81bce3569903afbd9c99
FredyRosero <faroseroc@unal.edu.co>
2021-12-31 16:33:39 -0500

    5-1

--- week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -1,10 +1,20 @@
 # Uses python3
 import sys
+import math
 
-def get_change(m):
-    #write your code here
-    return m // 4
+def get_change(money):
+    denominations = [1, 3, 4]
+    minCoins = [0] + [math.inf]*money
+
+    for i in range(1, money+1):
+        for j in denominations:
+            if i>=j:
+                coins = minCoins[i-j]+1
+                if coins < minCoins[i]:
+                    minCoins[i] = coins
+
+    return(minCoins[money])
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
+    m = int(sys.stdin.readline())
     print(get_change(m))
