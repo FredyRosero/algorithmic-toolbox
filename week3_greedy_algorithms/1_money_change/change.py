17288bd7077e49caa14380027bc13ece92bc8dc8
FredyRosero <faroseroc@unal.edu.co>
2021-12-31 01:13:48 -0500

    3-1

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -2,9 +2,11 @@
 import sys
 
 def get_change(m):
-    #write your code here
-    return m
+    a = int(m / 10)
+    b = int(m % 10 / 5)
+    c = int(m % 10 % 5)
+    return a+b+c
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
+    m = int(sys.stdin.readline())
     print(get_change(m))
